f9330713437b7a1f46b442f51932a3c74d0c2f7f
JohnnyPP <johnnnyppp@gmail.com>
2013-03-23 21:40:22 +0100

    Added second timer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,19 +22,33 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        int i = 0;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            
+
 
             DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            DispatcherTimer dispatcherTimer2 = new DispatcherTimer();
            
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick); 
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
             dispatcherTimer.Start();
+
+            dispatcherTimer2.Tick += new EventHandler(dispatcherTimer_Tick2);
+            dispatcherTimer2.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer2.Start();
           
+        }
+
+        private void dispatcherTimer_Tick2(object sender, EventArgs e)
+        {
+           
+            textBox3.Text = i.ToString();
+            i++;
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
